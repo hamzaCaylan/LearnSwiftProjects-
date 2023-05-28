ccc6001dc28d46c97536cbcb1b92c97e8481e11c
hamzaçaylan <hamzacaylan021@gmail.come>
2023-05-28 15:58:31 +0300

    arayüz hazırlandı

---
+++ LearnCoreData/DetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailsViewController.swift
+//  LearnCoreData
+//
+//  Created by Hamza Caylan on 28.05.2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var isimTextfielt: UITextField!
+    @IBOutlet weak var yazarTextField: UITextField!
+    @IBOutlet weak var fiyatTextField: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    @IBAction func kaydetButtonTiklandi(_ sender: Any) {
+    }
+    
+}
