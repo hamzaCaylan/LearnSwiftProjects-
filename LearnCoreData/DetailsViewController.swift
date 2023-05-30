3510b7ac348903eee9c2ae0d70245bb55156a379
hamzaçaylan <hamzacaylan021@gmail.come>
2023-05-30 11:46:07 +0300

    8.7 veriler kaydedildi

--- LearnCoreData/DetailsViewController.swift
+++ LearnCoreData/DetailsViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController,UIPickerViewDelegate,UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
 
     @IBOutlet weak var isimTextfielt: UITextField!
     @IBOutlet weak var yazarTextField: UITextField!
@@ -19,15 +20,55 @@ class DetailsViewController: UIViewController {
         let gestureRecognaizer = UITapGestureRecognizer(target: self, action: #selector(klavyiKapat))
         view.addGestureRecognizer(gestureRecognaizer)
         
+        imageView.isUserInteractionEnabled = true
+        let imageGestureReconizer = UITapGestureRecognizer(target: self, action: #selector(imageTiklandi))
+        imageView.addGestureRecognizer(imageGestureReconizer)
+    }
+    
+    @objc func imageTiklandi(){
+       let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true,completion: nil)
+        
     }
      
     @objc func klavyiKapat() {
         view.endEditing(true)
     }
     
-
-    
     @IBAction func kaydetButtonTiklandi(_ sender: Any) {
+        
+        let appDeleget = UIApplication.shared.delegate as! AppDelegate
+        let context = appDeleget.persistentContainer.viewContext
+        let liste = NSEntityDescription.insertNewObject(forEntityName: "Kitaplar", into: context)
+        
+        liste.setValue(isimTextfielt.text, forKey: "adi")
+        liste.setValue(yazarTextField.text, forKey: "yazar")
+        
+        if let fiyat = Int(fiyatTextField.text!){
+            liste.setValue(fiyat, forKey: "fiyat")
+        }
+        liste.setValue(UUID(), forKey: "uuId")
+        
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        liste.setValue(data, forKey: "image")
+        
+        do{
+            try context.save()
+            print("kaydettim")
+
+        }
+        catch{
+            print("sdadas")
+        }
+
     }
     
 }
